82f44d2be2facec19333478ce4cf6d38692b4d7c
patricia.oshiro@totvs.com.br <patricia.oshiro@totvs.com.br>
2018-07-16 23:09:38 -0300

    first commit

---
+++ Dapper/Cliente.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Dapper
+{
+    public class Cliente
+    {
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+        public string Endereco { get; set; }
+        public string Email { get; set; }
+        public DateTime DataHoraInclusao { get; set; }
+    }
+}
---
+++ Dapper/Program.cs
@@ -0,0 +1,91 @@
+﻿using Dapper.Dapper;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace Dapper
+{
+    class Program
+    {
+        static string strConexao = ConfigurationManager.ConnectionStrings["conexaoBeleza"].ConnectionString;
+
+        static void Main(string[] args)
+        {
+            Exemplo1();
+            Exemplo2();
+            Exemplo3();
+            Exemplo4();
+            Exemplo5();
+            Console.ReadKey();
+        }
+
+        private static void Exemplo1()
+        {
+            SqlConnection conexaoBD = new SqlConnection(strConexao);
+            conexaoBD.Open();
+
+            var resultado = conexaoBD.Query("Select * from Cliente");
+
+            Console.WriteLine("{0} - {1} - {2} ", "Código", "Nome do Contato", "Endereco do Cliente");
+            foreach (dynamic cliente in resultado)
+            {
+                Console.WriteLine("{0} - {1} - {2} ", cliente.Id, cliente.Nome, cliente.Endereco);
+            }
+            conexaoBD.Close();
+        }
+
+        private static void Exemplo2()
+        {
+            using (var conexaoBD = new SqlConnection(strConexao))
+            {
+                conexaoBD.Open();
+                IEnumerable clientes = conexaoBD.Query<Cliente>("Select * from Cliente");
+
+                Console.WriteLine("{0} - {1} - {2} ", "Código", "Nome do Cliente", "Email");
+                foreach (Cliente cliente in clientes)
+                {
+                    Console.WriteLine("{0} - {1} - {2}", cliente.Id, cliente.Nome, cliente.Email);
+                }
+            }
+        }
+
+        private static void Exemplo3()
+        {
+            using (SqlConnection cn = new SqlConnection(strConexao))
+            {
+                cn.Open();
+                Cliente person = cn.Get<Cliente>("EA427461-9FD2-4543-917B-0E969989DD52");
+                cn.Close();
+            }
+        }
+
+        private static void Exemplo4()
+        {
+            using (SqlConnection cn = new SqlConnection(strConexao))
+            {
+                cn.Open();
+                var predicate = Predicates.Field<Cliente>(f => f.Nome, Operator.Eq, "juca");
+                int count = cn.Count<Cliente>(predicate);
+                cn.Close();
+            }
+        }
+
+        private static void Exemplo5()
+        {
+            using (SqlConnection cn = new SqlConnection(strConexao))
+            {
+                cn.Open();
+                var predicate = Predicates.Field<Cliente>(f => f.DataHoraInclusao, Operator.Le, DateTime.Now);
+                IEnumerable<Cliente> list = cn.GetList<Cliente>(predicate);
+                foreach (var item in list)
+                {
+                    string nome = item.Email;
+                }
+                cn.Close();
+            }
+        }
+
+    }
+}
